674541d8c43bb8bc7b284781b220d517d72a930b
Olivier Racette <olivieracette@gmail.com>
2019-03-29 18:42:46 -0400

    Window loop
    Creates a 640*480 window that does nothing but quit on key down.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <ostream>
+#include <string>
 #include "SDL.h"
 
 using namespace std;
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+void logSDLError(ostream &os, const string &msg) {
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
 
 int main(int argc, char * argv []) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -9,6 +18,31 @@ int main(int argc, char * argv []) {
 	}
 	else {
 		cout << "SDL Initialized properly";
+	}
+
+
+	SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (!window) {
+		logSDLError(cout, "Window");
+	}
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if (!renderer) {
+		logSDLError(cout, "Renderer");
+	}
+
+	SDL_Event e;
+	bool quit = false;
+
+
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_KEYDOWN) {
+				quit = true;
+			}
+		}
 	}
 
 	return 0;
